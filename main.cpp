59854da872918fef4f1aeb0b0cc58043120a13ca
kamiji-tomoyuki <k023g0115@g.neec.ac.jp>
2024-09-30 11:40:37 +0900

    01_01_確認課題

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include <stdio.h>
 
+template <typename Type>
+Type Min(Type a, Type b) {
+    if (a < b) {
+        return a;
+    }
+    else {
+        return b;
+    }
+}
+
+template<>
+char Min<char>(char a, char b) {
+    printf("数字以外は代入出来ません\n");
+    return '\0';
+}
+
 int main() {
+    printf("%d\n", Min<int>(100, 10));
+    printf("%f\n", Min<float>(3.14f, 2.14f));
+    printf("%lf\n", Min<double>(2.14, 3.14));
 
-	char str[] = "あ";
-	printf("%s", str);
+    printf("%c\n", Min<char>('a', 'あ'));
 
-	return 0;
+    return 0;
 }
