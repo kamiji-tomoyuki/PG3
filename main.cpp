db9f66e68b7bc5e87a52c3e8f8cee81744407fb8
kamiji-tomoyuki <k023g0115@g.neec.ac.jp>
2024-12-20 17:19:05 +0900

    06_02

--- main.cpp
+++ main.cpp
@@ -1,5 +1,52 @@
-#include <stdio.h>
-int main() {
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
 
-	return 0;
+std::mutex mtx;
+std::condition_variable cv;
+bool ready1 = true, ready2 = false, ready3 = false;
+
+void thread1() {
+    std::unique_lock<std::mutex> lock(mtx);
+    cv.wait(lock, [] { return ready1; });
+
+    std::cout << "thread 1" << std::endl;
+
+    ready1 = false;
+    ready2 = true;
+    cv.notify_all();
+}
+
+void thread2() {
+    std::unique_lock<std::mutex> lock(mtx);
+    cv.wait(lock, [] { return ready2; });
+
+    std::cout << "thread 2" << std::endl;
+
+    ready2 = false;
+    ready3 = true;
+    cv.notify_all();
+}
+
+void thread3() {
+    std::unique_lock<std::mutex> lock(mtx);
+    cv.wait(lock, [] { return ready3; });
+
+    std::cout << "thread 3" << std::endl;
+
+    ready3 = false;
+    cv.notify_all();
+}
+
+int main() {
+    std::thread t1(thread1);
+    std::thread t2(thread2);
+    std::thread t3(thread3);
+
+    t1.join();
+    t2.join();
+    t3.join();
+
+    return 0;
 }
